a3688ee3c10980a35af7c30665ee424c4c9fa5a2
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-15 14:07:05 -0600

    add validations to prescription model

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -2,4 +2,7 @@ class Prescription < ActiveRecord::Base
   belongs_to :user
   belongs_to :patient
   belongs_to :medication
+
+  validates_presence_of :medication_id, :dosage, :schedule, :starts_on, :ends_on, :user_id, :patient_id
+
 end

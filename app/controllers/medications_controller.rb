d0ac93fb4a614ade28cb2a8c987c3070d98e61f3
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-15 13:52:04 -0600

    add prescriptions controller

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,4 +1,4 @@
-class Medications < ApplicationController
+class MedicationsController < ApplicationController
   def show
     @medication = Medication.find(params[:id])
     @prescriptions = @medication.prescriptions

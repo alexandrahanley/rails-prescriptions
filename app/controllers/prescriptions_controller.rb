c14e2958eec4e892729df8213d9685189cf5a9e0
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-15 14:01:50 -0600

    add create action to prescriptions controller,update patients show to reflect that info

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -3,4 +3,22 @@ class PrescriptionsController < ApplicationController
     @prescription = Prescription.new
     @patient = Patient.find(params[:patient_id])
   end
+
+  def create
+    @prescription = Prescription.new(prescription_params)
+    @patient = Patient.find(params[:patient_id])
+    @prescription.patient_id = @patient.id
+      if @prescription.save
+        redirect_to patient_path(@patient), notice: "Your prescription was created."
+      else
+        render :new
+      end
+  end
+
+private
+
+  def prescription_params
+    params.require(:prescription).permit(:medication_id, :dosage, :schedule, :starts_on, :ends_on, :patient_id)
+  end
+
 end

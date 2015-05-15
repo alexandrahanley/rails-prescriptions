82e48a826c4398f62a36c55fccf8b7bc16831cde
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-15 14:34:35 -0600

    user can see prescriptions on patient show page

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,5 +2,6 @@ class PatientsController < ApplicationController
   def show
     @patient = Patient.find(params[:id])
     @prescriptions = @patient.prescriptions
+    @medications = @patient.medications
   end
 end
--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -3,6 +3,6 @@ class Prescription < ActiveRecord::Base
   belongs_to :patient
   belongs_to :medication
 
-  validates_presence_of :medication_id, :dosage, :schedule, :starts_on, :ends_on, :user_id, :patient_id
+  validates_presence_of :medication_id, :dosage, :schedule, :starts_on, :ends_on, :patient_id
 
 end
